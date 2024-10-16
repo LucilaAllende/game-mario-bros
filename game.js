8447a918a0029ee367b49f73534eb4b169536882
LucilaAllende <abigail_lucii@hotmail.com>
2024-10-16 13:58:48 -0300

    Refactor game.js to update physics settings and add collectible items

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ import { initAudios, playAudio } from './audios.js'
 import { initSpritesheet } from './spritesheet.js'
 
 const config = {
+  autofocus: false,
   type: Phaser.AUTO,
   width: 256,
   height: 244,
@@ -12,7 +13,7 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 500 },
+      gravity: { y: 300 },
       debug: false
     }
   },
@@ -33,6 +34,58 @@ function preload () {
   initAudios(this)
 }
 
+function create () {
+  createAnimations(this)
+
+  this.add.image(100, 50, 'cloud').setScale(0.15).setOrigin(0, 0)
+
+  this.floor = this.physics.add.staticGroup()
+  this.floor.create(0, config.height - 16, 'floorbricks').setOrigin(0, 0.5).refreshBody()
+  this.floor.create(150, config.height - 16, 'floorbricks').setOrigin(0, 0.5).refreshBody()
+
+  this.mario = this.physics.add.sprite(50, 100, 'mario')
+    .setOrigin(0, 1)
+    .setCollideWorldBounds(true)
+    .setGravityY(300)
+
+  this.goomba = this.physics.add.sprite(120, config.height - 30, 'goomba')
+    .setOrigin(0, 1)
+    .setGravityY(300)
+    .setVelocityX(-50)
+
+  this.goomba.anims.play('goomba-walk', true)
+
+  this.collectibles = this.physics.add.staticGroup()
+  this.collectibles.create(150, 150, 'coin')
+    .anims.play('coin-idle', true)
+  this.collectibles.create(300, 150, 'coin')
+    .anims.play('coin-idle', true)
+
+  this.collectibles.create(200, config.height - 40, 'supermushroom')
+    .anims.play('supermushroom-idle', true)
+
+  this.physics.add.overlap(this.mario, this.collectibles, collectItem, null, this)
+
+  this.physics.world.setBounds(0, 0, 2000, config.height)
+  this.physics.add.collider(this.mario, this.floor)
+  this.physics.add.collider(this.goomba, this.floor)
+  this.physics.add.collider(this.mario, this.goomba, onHitGoomba, null, this)
+
+  this.cameras.main.setBounds(0, 0, 2000, config.height)
+  this.cameras.main.startFollow(this.mario)
+
+  this.keys = this.input.keyboard.createCursorKeys()
+}
+
+function update () {
+  const { mario } = this
+
+  checkControls(this)
+  if (mario.y >= config.height) {
+    killMario(this)
+  }
+}
+
 function onHitGoomba (mario, goomba) {
   if (mario.body.touching.down && goomba.body.touching.up) {
     goomba.anims.play('goomba-dead', true)
@@ -41,9 +94,6 @@ function onHitGoomba (mario, goomba) {
 
     playAudio('goomba-stomp', this)
     addToScore(200, mario, this)
-
-    mario.body.checkCollision.none = true
-    mario.setVelocityX(0)
 
     setTimeout(() => {
       goomba.destroy()
@@ -68,7 +118,7 @@ function killMario (game) {
   mario.setVelocityX(0)
 
   setTimeout(() => {
-    mario.setVelocityY(-200)
+    mario.setVelocityY(-250)
   }, 100)
 
   setTimeout(() => {
@@ -100,12 +150,14 @@ function collectItem (mario, item) {
     setTimeout(() => {
       mario.setDispalySize(18, 32)
       mario.body.setSize(18, 32)
+
       mario.body.setOffset(0, 0)
       mario.refreshBody()
+
+      this.anims.resumeAll()
       mario.isBlocked = false
       clearInterval(interval)
       this.physics.world.resume()
-      this.anims.resumeAll()
     }, 1000)
   }
 }
@@ -125,65 +177,11 @@ function addToScore (scoreToAdd, origin, game) {
       game.tweens.add({
         targets: scoreText,
         alpha: 0,
-        duration: 1000,
+        duration: 100,
         onComplete: () => {
           scoreText.destroy()
         }
       })
     }
   })
-}
-
-function create () {
-  createAnimations(this)
-
-  this.add.image(150, 10, 'cloud').setScale(0.15).setOrigin(0, 0)
-
-  this.floor = this.physics.add.staticGroup()
-  this.floor.create(0, config.height - 32, 'floorbricks').setOrigin(0, 0).refreshBody()
-  this.floor.create(150, config.height - 32, 'floorbricks').setOrigin(0, 0).refreshBody()
-
-  this.mario = this.physics.add.sprite(50, 100, 'mario')
-    .setOrigin(0, 1)
-    .setCollideWorldBounds(true)
-    .setGravityY(350)
-
-  this.goomba = this.physics.add.sprite(100, 100, 'goomba')
-    .setOrigin(0, 1)
-    .setCollideWorldBounds(true)
-    .setGravityY(350)
-    .setVelocityX(-50)
-
-  this.goomba.anims.play('goomba-walk', true)
-
-  this.collectibles = this.physics.add.staticGroup()
-  this.collectibles.create(200, 150, 'coin')
-    .setOrigin(0, 1)
-    .anims.play('coin-idle', true)
-  this.collectibles.create(250, 175, 'coin')
-    .setOrigin(0, 1)
-    .anims.play('coin-idle', true)
-
-  this.collectibles.create(200, config.height - 40, 'supermushroom').anims.play('supermushroom-idle', true)
-
-  this.physics.add.overlap(this.mario, this.collectibles, collectItem, null, this)
-
-  this.physics.world.setBounds(0, 0, 2000, config.height)
-  this.physics.add.collider(this.mario, this.floor)
-  this.physics.add.collider(this.goomba, this.floor)
-  this.physics.add.collider(this.mario, this.goomba, onHitGoomba, null, this)
-
-  this.cameras.main.setBounds(0, 0, 2000, config.height)
-  this.cameras.main.startFollow(this.mario)
-
-  this.keys = this.input.keyboard.createCursorKeys()
-}
-
-function update () {
-  const { mario } = this
-
-  checkControls(this)
-  if (mario.y >= config.height) {
-    killMario(this)
-  }
 }
