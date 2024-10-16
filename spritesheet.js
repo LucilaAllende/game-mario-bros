39f47ca63b6a80f275f023f21199731f6e5ec1d2
LucilaAllende <abigail_lucii@hotmail.com>
2024-10-16 12:23:03 -0300

    Add sprite sheet for Mario's grown state

--- spritesheet.js
+++ spritesheet.js
@@ -16,6 +16,12 @@ const INIT_SPRITESHEET = [
     url: 'assets/collectibles/coin.png',
     frameWidth: 16,
     frameHeight: 16
+  },
+  {
+    key: 'mario-grown',
+    url: 'assets/entities/mario-grown.png',
+    frameWidth: 18,
+    frameHeight: 32
   }
 ]
 
